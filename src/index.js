12214a4582de68bcdb6292d2023da1ca060bff31
interika <interika11@gmail.com>
2022-07-13 18:11:59 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let memo = []
+  let bracketsObj = bracketsConfig.reduce((acc, item) => {
+    acc[item[0]] = item[1]
+    return acc
+  }, {})
+
+  for (let i = 0; i < str.length; i++) {
+    let topEl = memo[memo.length - 1]
+    let current = str[i]
+    if (bracketsObj[topEl] === current) {
+      memo.pop()
+      continue;
+    }
+    if (bracketsObj[current]) {
+      memo.push(current)
+      continue
+    }
+    if (memo.length === 0) {
+      return false
+    }
+  }
+
+  return memo.length === 0
 }
